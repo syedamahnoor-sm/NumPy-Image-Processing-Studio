0bd887719ab9a1480a8fc56f0a8b637f20185112
Syeda Mahnoor <syedamahnoor9e@gmail.com>
2026-07-04 20:58:11 +0500

    Implement negative filter

--- filters.py
+++ filters.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def grayscale(image_array):
+    
+    """
+    Convert an RGB image to grayscale.
+    """
+    
     red = image_array[:, :, 0]
     green = image_array[:, :, 1]
     blue = image_array[:, :, 2]
@@ -8,3 +13,12 @@ def grayscale(image_array):
     gray = (0.299 * red) + (0.587 * green) + (0.114 * blue)
 
     return gray.astype(np.uint8)
+
+
+def negative(image_array):
+    
+    """
+    Convert an RGB image to negative.
+    """
+    
+    return (255 - image_array).astype(np.uint8)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+
 import numpy as np
 from PIL import Image
+
 from filters import grayscale
+from filters import negative
+
 
 #Load the image
 image = Image.open("images/input.jpg")
@@ -8,10 +13,44 @@ image = Image.open("images/input.jpg")
 #Convert the image to numpy array
 image_array = np.array(image)
 
-#Implement grayscale
+
+print("Image loaded successfully.\n")
+
+print(f"Shape       : {image_array.shape}")
+print(f"Data Type   : {image_array.dtype}")
+print(f"Dimensions  : {image_array.ndim}")
+print(f"Total Pixels: {image_array.shape[0] * image_array.shape[1]}")
+print(f"First Pixel : {image_array[0, 0]}")
+
+
+# Create Output Folder
+output_folder = "outputs"
+os.makedirs(output_folder, exist_ok=True)
+
+# Function to Save Image
+def save_image(image, filename):
+
+    filepath = os.path.join(output_folder, filename)
+
+    if os.path.exists(filepath):
+        print(f"{filename} already exists. Skipping...\n")
+        return
+
+    image.save(filepath)
+    print(f"{filename} saved successfully.\n")
+
+
+#GRAYSCALE FILTER
 gray_array = grayscale(image_array)
 
 gray_image = Image.fromarray(gray_array)
 gray_image.save("outputs/grayscale.jpg")
 
 print("Grayscale image saved successfully.")
+
+#NEGATIVE FILTER
+negative_array = negative(image_array)
+
+negative_image = Image.fromarray(negative_array)
+
+save_image(negative_image, "negative.jpg")
